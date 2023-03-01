277cdc717e597a6ee4f6c867ef22823f36aaaa60
vonb1910333 <vonb1910333@student.ctu.edu.vn>
2023-03-01 10:40:34 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/InputSearch.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    props: {
+        modelvalue: {
+            type: String, default: ""},
+    },
+        emits: ["submit", "update:modevalue"],
+
+        methods: {
+            updateModevalue(e) {
+                this.$emit("update:modevalue", e.target.value);
+            },
+            submit() {
+                this.$emit("submit");
+            },
+        },
+};
+</script>
+
+<template>
+    <div class="input-group">
+        <input type="text" class="form-control" placeholder="Nhập thông tin cần tìm"
+        :value="modelValue"
+        @input="updateModelValue"
+        @keyup.enter="submit"
+        />
+        <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="button" @click="submit">
+                <i class="fas fa-search"></i> Tìm kiếm
+            </button>
+        </div>
+    </div>
+</template>
